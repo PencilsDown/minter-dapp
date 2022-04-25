a42310f5b6b9fcac09277f3e0a6a2657cfa1baaa
Joe Sartor <josephwsartor@gmail.com>
2022-04-25 07:19:35 -0400

    switch to polygon

--- backend/src/config.js
+++ backend/src/config.js
@@ -41,7 +41,7 @@ const extraMetadata = {};
 // ** REQUIRED **
 const AUTH = process.env.NFTPORT_API_KEY; // Set this in the .env file to prevent exposing your API key when pushing to Github
 const LIMIT = 10; // Your API key rate limit
-const CHAIN = 'rinkeby'; // only rinkeby or polygon
+const CHAIN = 'polygon'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
 const CONTRACT_NAME = 'Eggs Of Knowledge';
@@ -50,7 +50,7 @@ const METADATA_UPDATABLE = false; // set to false if you don't want to allow met
 const OWNER_ADDRESS = '0xa6f928959B42cF4a57f330D839736dB8db2b18b7';
 const TREASURY_ADDRESS = '0xa6f928959B42cF4a57f330D839736dB8db2b18b7';
 const MAX_SUPPLY = 2500; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 9; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
